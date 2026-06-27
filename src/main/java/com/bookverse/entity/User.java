457805fc21a7278fe1470eff65e601c1ab2fcf19
TaiDuc1001 <phantaiduc2005@gmail.com>
2026-06-27 10:02:20 +0700

    fix: remove flyway

--- src/main/java/com/bookverse/entity/User.java
+++ src/main/java/com/bookverse/entity/User.java
@@ -31,7 +31,7 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(name = "password_hash", nullable = false)
--- src/main/resources/db/migration/V4__add_rag_columns.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE books
-    ADD COLUMN file_key VARCHAR(500),
-    ADD COLUMN cover_key VARCHAR(500),
-    ADD COLUMN last_indexed_at TIMESTAMPTZ;
